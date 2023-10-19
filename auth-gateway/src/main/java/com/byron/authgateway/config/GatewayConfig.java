a8d21b7f0c62cd21723043a3532c35e083ffde8d
ramzeta <ramiroperezsanz13@gmail.com>
2023-10-19 19:18:27 +0200

    update fix microservices and db

--- auth-gateway/src/main/java/com/byron/authgateway/config/GatewayConfig.java
+++ auth-gateway/src/main/java/com/byron/authgateway/config/GatewayConfig.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
-    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
-        return builder.routes()
-                .route("ms-users-route", r -> r.path("/v1/users/**")
-                        .uri("lb://ms-users"))
-                .route("ms-profiles-route", r -> r.path("/v1/profiles/**")
-                        .uri("lb://ms-profiles"))
-                .build();
-    }
+        @Bean
+        public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+                return builder.routes()
+                                .route("ms-users-route", r -> r.path("/v1/users/**")
+                                                .uri("lb://ms-users"))
+                                .route("ms-profiles-route", r -> r.path("/v1/profiles/**")
+                                                .uri("lb://ms-profiles"))
+                                .build();
+        }
 }
